7efea670ebff84b81a9b1b13a836e7d95cdb7cc6
MiAneko <mariya.serova2010@yandex.ru>
2022-12-01 14:03:59 +0300

    Fix CORS errors

---
+++ FuzzyInferenceSystemsBackend/src/main/kotlin/com/mianeko/fuzzyinferencesystemsbackend/swagger/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package com.mianeko.fuzzyinferencesystemsbackend.swagger
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.cors()
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", CorsConfiguration().applyPermitDefaultValues())
+        return source
+    }
+}
